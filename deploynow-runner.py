81f735efd66e937012305d5f015666f52512c5d0
Anushka Yohan <anushka.yohan@pearson.com>
2019-07-05 21:35:19 +0530

    Initial commit; after everything is working more or less :P

---
+++ deploynow-runner.py
@@ -0,0 +1,4 @@
+from deploynow.deploynow import main
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('README.md', 'r') as in_:
+    long_description = in_.read()
+
+setuptools.setup(
+    name='deploynow',
+    version='1.0.0',
+    author='Anushka Yohan',
+    author_email='anushka.yohan@pearson.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/AnushkaYohan/code-deploy-now',
+    packages=setuptools.find_packages(),
+    classifiers=["Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent"
+                 ]
+)
