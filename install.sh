03272af037974204ae52d54d3ffcaaabebfdd2ad
Anushka Yohan <anushka.yohan@pearson.com>
2019-07-08 19:09:27 +0530

    Remove install script

--- install.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/AnushkaYohan/code-deploy-now.git
-cd ./code-deploy-now/
-python setup.py install
-echo "Invoke deploynow module by running:\t python -m deploynow -h"
-cd ..
-rm -rf ./code-deploy-now/
