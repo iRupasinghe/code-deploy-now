b4196853885d38da6411997e796c448af09bb421
Rupasinghe, Iruka <Iruka.Rupasinghe@pearson.com>
2019-07-10 09:44:40 +0530

    Remodified setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open('README.md', 'r') as in_:
     long_description = in_.read()
 
 setuptools.setup(
     name='deploynow',
-    version='1.0.0',
     author='Anushka Yohan',
     author_email='anushka.yohan@pearson.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/AnushkaYohan/code-deploy-now',
@@ -15,7 +21,10 @@ setuptools.setup(
     classifiers=["Programming Language :: Python :: 3",
                  "License :: OSI Approved :: MIT License",
                  "Operating System :: OS Independent"
-                 ],
+    ],
     install_requires=['boto3'],
-    scripts=['scripts/deploynow']
+    scripts=['deploynow']
+    packages=find_packages(),
+    include_package_data=True,
+    version="1.5.0",
 )
